1941bfbc3454af78cfa4d03405daa4a0941884bd
Alexander Kargl <alexanderkargl@gmail.com>
2017-10-16 19:48:16 +0200

    Reject too large attachments

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ DATABASE = 'database.db'
 # TODO: point to correct path
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
-MAX_ATTACHMENT_LENGTH = 20 * 1024 * 1024
+MAX_CONTENT_LENGTH = 20 * 1024 * 1024
