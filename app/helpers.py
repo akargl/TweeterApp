1e38a40e33dbea0bcb1c4cc2c39652ac3a40dbd9
Robert Schilling <rschilling@student.tugraz.at>
2017-10-18 15:28:32 +0200

    Start with CSRF protection

--- app/helpers.py
+++ app/helpers.py
@@ -3,6 +3,8 @@ from functools import wraps
 from flask import g, request, redirect, url_for, abort
 from models import Session, User
 from app import app
+from werkzeug.security import safe_str_cmp
+from itsdangerous import URLSafeTimedSerializer, BadData, SignatureExpired
 
 
 def user_from_credentials():
@@ -22,8 +24,6 @@ def user_from_credentials():
     if not salt:
         return None
 
-    app.logger.debug('Found salt for user')
-
     _, hashed_password = User.create_hashed_password(salt, password)
     user = User.get_and_validate_user(username, hashed_password)
     return user
@@ -34,25 +34,58 @@ def user_from_session():
     return Session.active_user(g.session_token)
 
 
-def get_user():
-    session_user = user_from_session()
-    if session_user is not None:
-        app.logger.debug("Session found for '" + session_user.username + "'")
-        return session_user
-    credentials_user = user_from_credentials()
-    return credentials_user
-
-
 def authentication_required(admin=False, redirect_to_login=True):
     def _authentication_required(func):
         @wraps(func)
         def func_wrapper(*args, **kwargs):
-            user = get_user()
-            if user is None or (admin and not user.is_admin):
-                if redirect_to_login:
-                    return redirect(url_for('login', next=request.url))
-                return abort(httplib.UNAUTHORIZED)
+            user, csrf_token = user_from_session()
+            if user:
+                app.logger.debug("Login via session cookie for {:s}".format(user.username))
+                g.csrf_token = csrf_token
+                response = csrf_protection()
+                if response:
+                    return response
+            else:
+                # Fallback on HTTP Basic Auth
+                user = user_from_credentials()
+                if not user:
+                    if redirect_to_login:
+                        return redirect(url_for('login'))
+                    return abort(httplib.UNAUTHORIZED)
+
             g.user = user
+            if admin and not user.is_admin:
+                if redirect_to_login:
+                    return redirect(url_for('login'))
+                return abort(httplib.UNAUTHORIZED)
+
             return func(*args, **kwargs)
         return func_wrapper
     return _authentication_required
+
+
+def csrf_protection():
+    if request.method not in ['POST', 'PUT', 'DELETE']:
+        return
+
+    # Get the CSRF token
+    form_csrf_token = request.form.get('csrf-token')
+    if not form_csrf_token:
+        return abort(httplib.BAD_REQUEST)
+
+    session_csrf_token = g.get('csrf_token')
+    if not session_csrf_token:
+        return abort(httplib.BAD_REQUEST)
+
+    signer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        signer.loads(form_csrf_token, max_age=app.config['MAX_CSRF_TOKEN_AGE'])
+    except SignatureExpired:
+        from views import logout
+        return logout()
+    except BadData:
+        return abort(httplib.BAD_REQUEST)
+
+    match = safe_str_cmp(form_csrf_token, session_csrf_token)
+    if not match:
+        return abort(httplib.BAD_REQUEST)
--- config.py
+++ config.py
@@ -3,3 +3,6 @@ DATABASE = 'database.db'
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 MAX_CONTENT_LENGTH = 20 * 1024 * 1024
+# TODO: Is this the right way?
+SECRET_KEY = "my secret key"
+MAX_CSRF_TOKEN_AGE = 3600
