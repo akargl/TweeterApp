dd7764b3132dd7d7e88c6c10c8313699e104ff43
Robert Schilling <rschilling@student.tugraz.at>
2017-11-09 16:42:48 +0100

    Create secret keyfile if there is none

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from flask import Flask
 
 # Initialize the app
@@ -7,5 +9,17 @@ app = Flask(__name__, instance_relative_config=True)#
 from app import views
 
 
+def install_secret_key(app, filename='secret_key'):
+    filename = os.path.join(app.root_path, filename)
+    try:
+        app.config['SECRET_KEY'] = open(filename, 'rb').read()
+    except IOError:
+        app.logger.info('No Secret. Create a new one.')
+        with open(filename, 'wb') as f:
+            f.write(os.urandom(32))
+            app.config['SECRET_KEY'] = open(filename, 'rb').read()
+
+
 # Load the config file
 app.config.from_object('config')
+install_secret_key(app)
