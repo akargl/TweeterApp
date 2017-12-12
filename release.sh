fca2114ed96f00d25ab13995551c016da386e0ec
Robert Schilling <rschilling@student.tugraz.at>
2017-12-12 19:45:22 +0100

    Requirements need to be in the release

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ mkdir -p $RELEASE_FOLDER
 cp README.md $RELEASE_FOLDER
 cp Dockerfile $RELEASE_FOLDER
 cp docker-compose.yml $RELEASE_FOLDER
+cp requirements.txt $RELEASE_FOLDER
 cp *.py $RELEASE_FOLDER
 cp -r app $RELEASE_FOLDER
 mkdir -p $RELEASE_FOLDER/tests/test_data
